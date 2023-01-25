5d372f8e85f2263f3efc296b7014e5166416e268
amartinez <amartinez@wawand.co>
2023-01-25 13:46:35 -0500

    feat: added one callback to reset the inputs and updated the existing one to do some DOM manipulation

--- prog/callbacks/callbacks.go
+++ prog/callbacks/callbacks.go
@@ -6,19 +6,37 @@ import (
 )
 
 func HasRepeatedChars(this js.Value, args []js.Value) any {
-	if len(args) > 1 {
-		fmt.Println("This callback only needs a single argument")
+	if len(args) > 2 {
+		fmt.Println("This callback only requires two arguments. One for the id of the input and another one for its result")
 		return nil
 	}
 
-	val := args[0].String()
+	resId := args[1].String()
 
-	r := hasRepeatedChars(val)
+	valId := args[0].String()
+
+	inp := js.Global().Get("document").Call("getElementById", valId).Get("value").String()
+	r := hasRepeatedChars(inp)
 	if r {
-		fmt.Println("Yes, it does")
+		fmt.Println("Yes, there is at least character repeating once")
+		updateResultBoxByID(resId, "Yes, there is at least character repeating once")
+
 		return true
 	}
 
-	fmt.Println("No, it does not")
+	fmt.Println("No, none of the characters is repeating")
+	updateResultBoxByID(resId, "No, none of the characters is repeating")
 	return false
+}
+
+func Clear(this js.Value, args []js.Value) any {
+	for _, arg := range args {
+		if arg.IsUndefined() || arg.IsNull() {
+			continue
+		}
+
+		updateResultBoxByID(arg.String(), "")
+	}
+
+	return nil
 }
