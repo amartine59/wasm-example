e8037648abaceb7922d5adac10ffa7c3c1efa73c
amartinez <amartinez@wawand.co>
2023-01-25 14:04:30 -0500

    feat: simplifying server

--- server.go
+++ server.go
@@ -2,20 +2,18 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
-	"strings"
 )
 
 func main() {
-	fs := http.FileServer(http.Dir("./"))
 	fmt.Println("Starting server on 8080...")
 
-	http.ListenAndServe(":8080", http.HandlerFunc(
-		func(w http.ResponseWriter, r *http.Request) {
-			if strings.HasSuffix(r.URL.Path, ".wasm") {
-				w.Header().Set("content-type", "application/wasm")
-			}
-
-			fs.ServeHTTP(w, r)
-		}))
+	log.Fatal(
+		http.ListenAndServe(":8080",
+			http.FileServer(
+				http.Dir("./"),
+			),
+		),
+	)
 }
