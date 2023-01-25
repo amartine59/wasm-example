bc531f2385eb36be226d14321258d720fb343dc8
amartinez <amartinez@wawand.co>
2023-01-25 10:31:23 -0500

    feat: changed wasm module to be executed as soon as the page loads

--- go.js
+++ go.js
@@ -3,14 +3,8 @@ const go = new Go();
 let mod, wasm;
 
 WebAssembly.instantiateStreaming(fetch("wasm.wasm"),go.importObject).then(
-    result => {
+    async result => {
     mod = result.module;
     wasm = result.instance;
-});
-
-
-async function run() {
-    console.clear();
     await go.run(wasm);
-    wasm = await WebAssembly.instantiate(mod,go.importObject);
-}
+});
